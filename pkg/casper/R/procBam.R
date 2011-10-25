a14d185c01eee8bf56783d193c77d3948154899d
cstephan (PhD ) <camille.stephan@irbbarcelona.org>
2011-10-25 08:51:00 +0000

    fixed chromosome names bug in procBam
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/casper@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 753612a39eb6ad4b7895d73a83b79dcb8971b54e

--- pkg/casper/R/procBam.R
+++ pkg/casper/R/procBam.R
@@ -21,12 +21,19 @@ procBam<-function(bamFileName, samtools, chrom, bam, parallel){
     
   #process lengths RangedData
     data[[1]]<-lapply(data[[1]][1:length(data[[1]])-1], function(x) x[1:data[[1]][[length(data[[1]])]]])
-    data[[1]]<-RangedData(IRanges(start=ifelse(data[[1]][[1]]<data[[1]][[2]], data[[1]][[1]], data[[1]][[2]]), end=ifelse(data[[1]][[1]]>data[[1]][[2]], data[[1]][[1]], data[[1]][[2]])), space=paste("chr", data[[1]][[5]], sep=""), id=data[[1]][[4]], flag=data[[1]][[3]], rid=data[[1]][[6]])
-  
+      if(sum(grepl(unique(data[[1]][[5]]), "chr"))>0) {
+          data[[1]]<-RangedData(IRanges(start=ifelse(data[[1]][[1]]<data[[1]][[2]], data[[1]][[1]], data[[1]][[2]]), end=ifelse(data[[1]][[1]]>data[[1]][[2]], data[[1]][[1]], data[[1]][[2]])), space=data[[1]][[5]], id=data[[1]][[4]], flag=data[[1]][[3]], rid=data[[1]][[6]])
+      } else {
+        data[[1]]<-RangedData(IRanges(start=ifelse(data[[1]][[1]]<data[[1]][[2]], data[[1]][[1]], data[[1]][[2]]), end=ifelse(data[[1]][[1]]>data[[1]][[2]], data[[1]][[1]], data[[1]][[2]])), space=paste("chr", data[[1]][[5]], sep=""), id=data[[1]][[4]], flag=data[[1]][[3]], rid=data[[1]][[6]])
+      }
   #Process reads RangedData
-    data[[2]]<-lapply(data[[2]][1:length(data[[2]])-1], function(x) x[1:data[[2]][[length(data[[2]])]]])
-    data[[2]]<-lapply(data[[2]], function(x) x[data[[2]][[7]]==data[[2]][[8]]])
-    data[[2]]<-RangedData(IRanges(start=ifelse(data[[2]][[1]]<data[[2]][[3]], data[[2]][[1]], data[[2]][[3]]), end=ifelse(data[[2]][[1]]<data[[2]][[3]], data[[2]][[4]], data[[2]][[2]])), space=paste("chr", data[[2]][[7]], sep=""), flag1=data[[2]][[5]], flag2=data[[2]][[6]])
+      data[[2]]<-lapply(data[[2]][1:length(data[[2]])-1], function(x) x[1:data[[2]][[length(data[[2]])]]])
+      data[[2]]<-lapply(data[[2]], function(x) x[data[[2]][[7]]==data[[2]][[8]]])
+      if(sum(grepl(unique(data[[2]][[7]]), "chr"))>0) {
+    data[[2]]<-RangedData(IRanges(start=ifelse(data[[2]][[1]]<data[[2]][[3]], data[[2]][[1]], data[[2]][[3]]), end=ifelse(data[[2]][[1]]<data[[2]][[3]], data[[2]][[4]], data[[2]][[2]])), data[[2]][[7]], flag1=data[[2]][[5]], flag2=data[[2]][[6]])
+      } else {
+          data[[2]]<-RangedData(IRanges(start=ifelse(data[[2]][[1]]<data[[2]][[3]], data[[2]][[1]], data[[2]][[3]]), end=ifelse(data[[2]][[1]]<data[[2]][[3]], data[[2]][[4]], data[[2]][[2]])), space=paste("chr", data[[2]][[7]], sep=""), flag1=data[[2]][[5]], flag2=data[[2]][[6]])
+      }
     names(data)<-c("reads", "frags")
   } else {
     if(bam>0){
