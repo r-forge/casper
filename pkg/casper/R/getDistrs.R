76fc8d6116d3b18ab1e43fd959307a7ef616f33b
drossell (David Rossell) <rosselldavid@gmail.com>
2012-02-10 13:20:41 +0000

    Faster getDistrs, adjusted bug
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/casper@22 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 4025359d0f0fc8636fd7e088de7f5727c4485899

--- pkg/casper/R/getDistrs.R
+++ pkg/casper/R/getDistrs.R
@@ -9,7 +9,7 @@ startsReadsTx<-function(ovfr, frags, tx, exonsRD){
     ee<-rev(ee)
   }
   if(!is.unsorted(ss)){
-    pos<-unlist(ovfr[as.character(tx$exon_id)])
+    pos<-unlist(ovfr[as.character(unlist(tx$exon_id))])
     stff<-start(frags)[pos]
     if(length(stff)>0){
       or<-order(stff)
@@ -30,43 +30,62 @@ startsReadsTx<-function(ovfr, frags, tx, exonsRD){
   res
 }
 
-getDistrs<-function(txs, exons, frags, mc.cores=1){
+firstBamReads <- function(bam, nreads) {
+  id <- unique(bam$qname[1:nreads])
+  sel <- bam$qname %in% id
+  lapply(bam,function(z) z[sel])
+}
+
+getDistrs<-function(txs, exons, bam, nreads=4*10^6){
   
-  #Find fragment length distribution for fragments aligning to exons larger than 1000 bases
+  #Format exons as RangeData
   cat("Calculating fragment length distribution\n")
   exonsRD<-as.data.frame(exons@unlistData)
-  exonsRD<-exonsRD[,1:6]
+  exonsRD<-exonsRD[,-7]
   colnames(exonsRD)[1]<-"space"
   exonsRD<-RangedData(exonsRD)
-  fragsL<-frags[space(frags) %in% space(exonsRD),]
+  
+  #Select a sample of reads
+  bam <- firstBamReads(bam, nreads=nreads)
+
+  #Find fragment length distribution for fragments aligning to exons larger than 1000 bases
+  if (!all(c('qname','rname','qwidth','pos','mpos') %in% names(bam))) stop('bam must contain elements qname, rname, qwidth, pos, mpos')
+  d <- bam$mpos - bam$pos
+  sel <- d<0; n <- bam$qname[sel]; sp <- bam$rname[sel]; en <- bam$pos[sel]+bam$qwidth[sel]-1
+  sel <- d>0; st <- bam$pos[sel]; names(st) <- bam$qname[sel]; st <- st[n]
+  sel <- st<en; st <- st[sel]; en <- en[sel]; sp <- sp[sel]
+  frags <- RangedData(IRanges(start=st,end=en),space=sp)
+  n <- names(frags)[names(frags) %in% names(exonsRD)]
+  fragsL<-frags[n]
   fragsL<-subsetByOverlaps(fragsL, exonsRD[width(exonsRD)>1000,], type="within")
-  ld<-width(fragsL)
-  ld<-table(ld)
+  ld<-table(width(fragsL))
+  ld <- ld[ld/sum(ld) > 0.0001]
 
   #Find fragment start distribution for fragments aligning to transcripts in genes with only one annotated tx
 
   cat("Calculating fragment start distribution\n")
-  txs<-RangedData(txs)
+  txs <- RangedData(ranges=ranges(txs),space=seqnames(txs),strand=strand(txs),values(txs))
   oneTx<- unlist(txs$gene_id) %in% names(which(table(unlist(txs$gene_id))==1))
   oneTx<-txs[oneTx,]
   oneTx<-subsetByOverlaps(oneTx, frags)
   exonsRD<-subsetByOverlaps(exonsRD, oneTx)
   over<-findOverlaps(frags, exonsRD, type="within")
-  ovfr<-split(queryHits(over), exonsRD$exon_id[subjectHits(over)])
-  
-  if(mc.cores>1) require(multicore)
-  if(mc.cores>1) {
-    if ('multicore' %in% loadedNamespaces()) {
-      stPerTx<-mclapply(1:nrow(oneTx), function(x) startsReadsTx(ovfr, frags, oneTx[x,], exonsRD) , mc.cores=mc.cores)
-    } else stop('multicore library has not been loaded!')
-  } else  {
-    stPerTx<-lapply(1:nrow(oneTx), function(x) startsReadsTx(ovfr, frags, oneTx[x,], exonsRD))
-  }
 
-  stPerTx<-stPerTx[!is.na(stPerTx)]
-  res<-lapply(stPerTx, function(x) x$res)
-  wi<-lapply(stPerTx, function(x) x$wi)
+  txid <- cumsum(exonsRD$exon_rank==1)
+  exonsRD$exstnogap <- 1+unlist(tapply(width(exonsRD),INDEX=txid,function(z) c(0,cumsum(z[-length(z)]))))
+  exonsRD$txlength <- unlist(tapply(width(exonsRD),INDEX=txid,function(z) rep(sum(z),length(z))))
+   
+  exstnogap <- exonsRD$exstnogap[subjectHits(over)]
+  txlength <- exonsRD$txlength[subjectHits(over)]
+  exst <- start(exonsRD)[subjectHits(over)]
+  exen <- end(exonsRD)[subjectHits(over)]
+  readst <- start(frags)[queryHits(over)]
+  readen <- end(frags)[queryHits(over)]
+  str <- exonsRD$strand[subjectHits(over)]
+  stDis <- double(length(readst))
+  sel <- str=='+'; stDis[sel] <- (exstnogap[sel]+readst[sel]-exst[sel])/txlength[sel]
+  sel <- str=='-'; stDis[sel] <- (exstnogap[sel]+exen[sel]-readen[sel])/txlength[sel]
   
   res<-unlist(res)
-  list(lenDis=ld, stDis=res)
+  list(lenDis=ld, stDis=stDis)
 }
