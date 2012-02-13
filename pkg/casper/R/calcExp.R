ee57f439125cb90dcb362d05696251a723c986ef
drossell (David Rossell) <rosselldavid@gmail.com>
2012-02-13 14:49:34 +0000

    Updated calcExp.R, added relIsoform.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/casper@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: ea5142ade8ed852588feda7d9b773a245c0df9f3

--- pkg/casper/R/calcExp.R
+++ pkg/casper/R/calcExp.R
@@ -10,9 +10,18 @@ calcExp<-function(distrs, genomeDB, pc){
   exp<-as.matrix(exp)
   featureData<-as.numeric(unlist(genomeDB$txs@elementMetadata$gene_id))
   names(featureData)<-unlist(genomeDB$txs@elementMetadata$tx_name)
-    featureData<-as.data.frame(as.matrix(featureData[rownames(exp)]))
+  featureData<-as.data.frame(as.matrix(featureData[rownames(exp)]))
   metadata <- data.frame(labelDescription = "gene_id", row.names="gene_id")
   featureData<-new("AnnotatedDataFrame", data=featureData, varMetadata=metadata)
-  exp<-new("ExpressionSet", exprs=exp, featureData=featureData, annotation="entrez")
+  exp<-new("ExpressionSet", exprs=exp, featureData=featureData, annotation="refseq")
+  names(fData(exp)) <- 'entrezid'
+  #Compute rpkm
+  exons <- unlist(genomeDB$exons)
+  geneid <- cumsum(values(exons)$exon_rank==1)
+  txlength <- tapply(width(exons),geneid,FUN=sum)
+  names(txlength) <- names(genomeDB$exons)
+  fData(exp)$txlength <- txlength[featureNames(exp)]
+  rpkm <- matrix(10^9 * exprs(exp)[,] / fData(exp)$txlength, nrow=nrow(exp), ncol=ncol(exp))
+  exp<-new("ExpressionSet", exprs=rpkm, featureData=featureData(exp), annotation="refseq")
   exp
 }
