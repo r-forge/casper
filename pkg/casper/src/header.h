dde4c35a23987769e94fba9985b6ef1cdc90c022
cstephan (PhD ) <camille.stephan@irbbarcelona.org>
2012-02-15 11:31:07 +0000

    Updated header.h
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/casper@31 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 0f0f2ba39125bbe69f193dd1cb3e81cd7f1fb236

--- pkg/casper/src/header.h
+++ pkg/casper/src/header.h
@@ -16,4 +16,20 @@ typedef struct read_t {
 	int nreads;
 } read_t;
 
+typedef struct path_t {
+	int len;
+	char *path;
+	int *exons;
+	int *starts;
+    int *rids;
+    int *tx;
+    int *rnk;
+	int pos;
+	int nexon;
+	int count;
+	int incomp;
+	int island;
+	char name[50];
+} path_t;
+
 int verbose;
