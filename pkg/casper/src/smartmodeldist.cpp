74de3964912d228b73f36d71193b0a53c88bb5c6
makro (Manuel K) <kroissm@in.tum.de>
2012-04-03 14:39:34 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/casper@98 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    Former-commit-id: 1c036a9a0178ce3e8d685183d8b72a1dac960c10

--- pkg/casper/src/smartmodeldist.cpp
+++ pkg/casper/src/smartmodeldist.cpp
@@ -128,7 +128,6 @@ void SmartModelDist::buildrmtable()
 Variant* SmartModelDist::makevar()
 {
 	int rv = runifdisc(0, center->items.size() - 1);
-	Variant* var = center->items[rv];
 
 	vector<Exon*>* nex = new vector<Exon*>();
 	for (unsigned int i = 0; i < frame->exons.size(); i++)
