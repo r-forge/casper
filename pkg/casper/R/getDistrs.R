ba146eeb49f7936cff3a63e5a4f49496effb5134
cstephan (PhD ) <camille.stephan@irbbarcelona.org>
2012-05-31 07:52:36 +0000

    getDistrs.R with Kaplan-Meier for read start distribution
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/casper@132 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 9fac1a353b376db2aa136d9e738922aee999046c

--- pkg/casper/R/getDistrs.R
+++ pkg/casper/R/getDistrs.R
@@ -1,3 +1,20 @@
+startDist <- function(st,fragLength,txLength) {
+                                        # Estimate relative start distribution under left­truncation (st < 1 ­ fragLength/txLength)
+                                        # ­ st: relative start (i.e. start/txLength)
+                                        # ­ fragLength: fragment length
+                                        # ­ txLength: transcript length
+                                        # Output: cumulative probability function (actually, a linear interpolation)
+  require(survival)
+  trunc <- 1-fragLength/txLength
+  sel <- trunc>st
+  trunc <- trunc[sel]
+  st <- st[sel]
+  fit <- summary(survfit(Surv(time=1-trunc,time2=1-st,event=rep( TRUE,length(st))) ~ 1))
+  s <- 1-fit$time
+  pcum <- fit$surv
+  f <- approxfun(s,pcum)
+  return(f)
+}
 
 firstBamReads <- function(bam, nreads) {
   if (nreads < length(bam$qname)) {
@@ -38,7 +55,6 @@ getDistrs<-function(DB, bam, nreads=4*10^6){
   oneTx <- DB@transcripts[sel]
   oneTx <- unlist(oneTx, recursive=F)
   names(oneTx) <- sub("[0-9]+\\.", "", names(oneTx))
-  
   oneExons <- exonsRD[exonsRD$id %in% unlist(oneTx),]
   oneExons <- as.data.frame(oneExons)
   rownames(oneExons) <- oneExons$id
@@ -59,22 +75,6 @@ getDistrs<-function(DB, bam, nreads=4*10^6){
   oneExons <- RangedData(oneExons)
   over <- findOverlaps(frags, oneExons, type="within")
   
-  #tx <- Exons[names(oneTx)]
-  #ExonsRD <- RangedData(tx@unlistData)  
-  #over1 <- findOverlaps(frags, ExonsRD, type="within")
-  #txid <- cumsum(ExonsRD$exon_rank==1)
-  #ExonsRD$exstnogap <- 1+unlist(tapply(width(ExonsRD),INDEX=txid,function(z) c(0,cumsum(z[-length(z)]))))
-  #ExonsRD$txlength <- unlist(tapply(width(ExonsRD),INDEX=txid,function(z) rep(sum(z),length(z))))
-  
-  ##to be removed
-  #exstnogap1 <- ExonsRD$exstnogap[subjectHits(over1)]
-  #txlength1 <- ExonsRD$txlength[subjectHits(over1)]
-  #exst1 <- start(ExonsRD)[subjectHits(over1)]
-  #exen1 <- end(ExonsRD)[subjectHits(over1)]
-  #readst1 <- start(frags)[queryHits(over1)]
-  #readen1 <- end(frags)[queryHits(over1)]
-  #str1 <- as.character(ExonsRD$strand[subjectHits(over1)])
-  
   exstnogap <- oneExons$exstnogap[subjectHits(over)]
   txlength <- oneExons$txlength[subjectHits(over)]
   exst <- start(oneExons)[subjectHits(over)]
@@ -82,28 +82,13 @@ getDistrs<-function(DB, bam, nreads=4*10^6){
   readst <- start(frags)[queryHits(over)]
   readen <- end(frags)[queryHits(over)]
   str <- oneExons$strand[subjectHits(over)]
-
+  frlen <- width(frags)[queryHits(over)]
+  
   stDis <- double(length(readst))
   sel <- str=='+';  stDis[sel] <- (exstnogap[sel]+readst[sel]-exst[sel])/txlength[sel]
   sel <- str=='-'; stDis[sel] <- (exstnogap[sel]+exen[sel]-readen[sel])/txlength[sel]
 
-  #sel <- str=='+';
-  #stDisP <- double(sum(sel)); stDisP <- (exstnogap[sel]+readst[sel]-exst[sel])/txlength[sel]
-  #sel <- str=='-';
-  #stDisM <- double(sum(sel)); stDisM <- (exstnogap[sel]+exen[sel]-readen[sel])/txlength[sel]
-  #sel <- str1=='+';
-  #stDisP1 <- double(sum(sel)); stDisP1 <- (exstnogap1[sel]+readst1[sel]-exst1[sel])/txlength1[sel]
-  #sel <- str1=='-';
-  #stDisM1 <- double(sum(sel)); stDisM1 <- (exstnogap1[sel]+exen1[sel]-readen1[sel])/txlength1[sel]
-  
-  
-  #x11()
-  #plot(density(stDis), ylim=c(0,2))
-  #lines(density(distrs$stDis), col="red")
-  #lines(density(stDisM), col="red")
-  #plot(density(stDisP1), ylim=c(0,2), col="green")
-  #lines(density(stDisM1), col="blue")
-
+  stDis <- startDist(stDis, frlen, txlength)
   
   list(lenDis=ld, stDis=stDis)
 }
