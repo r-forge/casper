e90aa22cfc9d5eddf3811f0a888053b5d3f3e924
cstephan (PhD ) <camille.stephan@irbbarcelona.org>
2012-11-26 13:52:33 +0000

    fixed bug in procBam
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/casper@218 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 22f71059cec2eb77db2b06bc9f3a2d9db0b3619a

--- pkg/casper/R/procBam.R
+++ pkg/casper/R/procBam.R
@@ -14,7 +14,7 @@ uniquifyQname<-function(bam, seed=1){
   qname <- vector(mode='character', length=length(bam$qname))
   dups <- .Call("uniqQname", bam$qname, length(bam$qname), bam$pos, bam$mpos, qname)
   bam$qname <- dups[[1]]
-  if(length(dups)>0){
+  if(length(dups[[2]])>0){
     probPos<-bam$qname %in% dups[[2]]
     probID<-paste(bam$qname[probPos], bam$pos[probPos])
     set.seed(seed)
@@ -39,50 +39,51 @@ nbReads <- function(bam0) {
     sum(tab*count)
 }
 
+procB <- function(bam, strnd, seed=1){
+  lev <- levels(bam$strand)
+  bam$rname<-as.character(bam$rname)
+  bam<-uniquifyQname(bam, seed)
+  cat("Calculating total number of reads...\n")
+  nreads<-nbReads(bam)+10
+  cat("done.\nProcessing cigars and building read's object...\n")
+  len=vector(mode="integer", length=nreads)
+  flag=vector(mode="integer", length=nreads)
+  strs=vector(mode="integer", length=nreads)
+  rid=vector(mode="integer", length=nreads)
+  key=vector(mode="character", length=nreads)
+  chrom=vector(mode="character", length=nreads)
+  strand=vector(mode="integer", length=nreads)
+  data<-.Call("procBam", bam$qname, bam$flag,  bam$rname, bam$pos, bam$cigar, as.numeric(bam$strand), length(bam$pos), nreads, len, strs, flag, key, chrom, rid, strand)
+  data[[7]] <- lev[data[[7]]]
+  sel<-data[[1]]!=0 & !is.na(data[[3]])
+  data<-lapply(data, "[", sel)
+  data[[length(data)+1]] <- rep(strnd, length(data[[1]]))
+  cat("done.\n")
+  ans<-buildRD(data)
+  id <- ans$id
+  idnum <- rep(0,length(id))
+  idnum[which(id[-length(id)] != id[-1])+1] <- 1
+  idnum <- cumsum(idnum)
+  ans$id <- idnum
+  ans
+}
+
 procBam<-function(bam, stranded=FALSE, seed=1){
   require(IRanges)
-  procB <- function(bam, strnd){
-    lev <- levels(bam$strand)
-    bam$rname<-as.character(bam$rname)
-    bam<-uniquifyQname(bam, seed)
-    cat("Calculating total number of reads...\n")
-    nreads<-nbReads(bam)+10
-    cat("done.\nProcessing cigars and building read's object...\n")
-    len=vector(mode="integer", length=nreads)
-    flag=vector(mode="integer", length=nreads)
-    strs=vector(mode="integer", length=nreads)
-    rid=vector(mode="integer", length=nreads)
-    key=vector(mode="character", length=nreads)
-    chrom=vector(mode="character", length=nreads)
-    strand=vector(mode="integer", length=nreads)
-    data<-.Call("procBam", bam$qname, bam$flag,  bam$rname, bam$pos, bam$cigar, as.numeric(bam$strand), length(bam$pos), nreads, len, strs, flag, key, chrom, rid, strand)
-    data[[7]] <- lev[data[[7]]]
-    sel<-data[[1]]!=0 & !is.na(data[[3]])
-    data<-lapply(data, "[", sel)
-    data[[length(data)+1]] <- rep(strnd, length(data[[1]]))
-    cat("done.\n")
-    ans<-buildRD(data)
-    id <- ans$id
-    idnum <- rep(0,length(id))
-    idnum[which(id[-length(id)] != id[-1])+1] <- 1
-    idnum <- cumsum(idnum)
-    ans$id <- idnum
-    ans
-  }
   if(stranded) {
     minus <- bam$tag$XS=='-'
     if(sum(minus)>0){
       minus <- lapply(bam, '[', minus)
-      minus <- procB(minus, "-")
+      minus <- procB(minus, "-", seed=seed)
     } else minus <- RangedData(IRanges(0,0), space=NULL)
     plus <- bam$tag$XS=='+'
     if(sum(plus)>0){
       plus <- lapply(bam, '[', plus)
-      plus <- procB(plus, "+")
+      plus <- procB(plus, "+", seed=seed)
     } else plus <- RangedData(IRanges(0,0), space=NULL)
     ans <- list(plus=plus, minus=minus, stranded=TRUE)
   } else {
-    pbam <- procB(bam, "*")
+    pbam <- procB(bam, "*", seed=seed)
     ans <- list(pbam=pbam, stranded=FALSE)
   }
   new("procBam",pbam=ans$pbam,stranded=ans$stranded)
