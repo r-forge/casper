37575a3c67a140096639c74009e31c326adcd7ee
cstephan (PhD ) <camille.stephan@irbbarcelona.org>
2013-04-29 16:16:07 +0000

    wrapKnown preschedule=FALSE to free memory after each cromosome
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/casper@251 edb9625f-4e0d-4859-8d74-9fd3b1da38cb
    
    Former-commit-id: 00e504f6eb55dee97588912222edd7e420152de6

--- pkg/casper/R/wrapKnown.R
+++ pkg/casper/R/wrapKnown.R
@@ -9,72 +9,74 @@ mergePCWr <- function(x, genomeDB){
 }
 
 mergeDisWr <- function(distrs, pcs){
-    minlen <- min(unlist(lapply(distrs, function(x) min(as.numeric(names(x@lenDis))))))
-      maxlen <- max(unlist(lapply(distrs, function(x) max(as.numeric(names(x@lenDis))))))
-      tmp <- lapply(distrs, function(x){
-            y <- x@lenDis
-                tmp <- vector(mode='numeric', length=maxlen-minlen+1)
-                names(tmp) <- minlen:maxlen
-                tmp[names(y)] <- y
-                tmp
-          } )
-      tmp <- do.call(cbind, tmp)
-      tmp <- as.array(rowSums(tmp))
-      distr <- new('readDistrs', lenDis=tmp)
-      th <- seq(0,1,length=10000)
-      tmp <- lapply(1:length(distrs), function(x){
-            all <- distrs[[x]]@stDis(th)*sum(getNreads(pcs[[x]]))
-          }
-                      )
-      tmp <- do.call(cbind, tmp)
-      tmp <- rowMeans(tmp)
-      tmp <- tmp/tmp[length(tmp)]
-      tmp <- approxfun(th, tmp)
-      distr@stDis <- tmp
-      distr
+  minlen <- min(unlist(lapply(distrs, function(x) min(as.numeric(names(x@lenDis))))))
+  maxlen <- max(unlist(lapply(distrs, function(x) max(as.numeric(names(x@lenDis))))))
+  tmp <- lapply(distrs, function(x){
+    y <- x@lenDis
+    tmp <- vector(mode='numeric', length=maxlen-minlen+1)
+    names(tmp) <- minlen:maxlen
+    tmp[names(y)] <- y
+    tmp
+  } )
+  tmp <- do.call(cbind, tmp)
+  tmp <- as.array(rowSums(tmp))
+  distr <- new('readDistrs', lenDis=tmp)
+  th <- seq(0,1,length=10000)
+  tmp <- lapply(1:length(distrs), function(x){
+    all <- distrs[[x]]@stDis(th)*sum(getNreads(pcs[[x]]))
   }
+                )
+  tmp <- do.call(cbind, tmp)
+  tmp <- rowMeans(tmp)
+  tmp <- tmp/tmp[length(tmp)]
+  tmp <- approxfun(th, tmp)
+  distr@stDis <- tmp
+  distr
+}
 
 
-wrapKnown <- function(bamFile, verbose=FALSE, seed=1, mc.cores.int=1, mc.cores=1, genomeDB, readLength, rpkm=TRUE, priorq=2, priorqGeneExpr=2, citype='none', niter=10^3, burnin=100) {
+
+wrapKnown <- function(bamFile, verbose=FALSE, seed=1, mc.cores.int=1, mc.cores=1, genomeDB, readLength, rpkm=TRUE, priorq=2, priorqGeneExpr=2, citype='none', niter=10^3, burnin=100, keep.pbams=FALSE) {
   what <- scanBamWhat(); what <- what[!(what %in% c('seq','qual', 'flag'))]
   t <- scanBamHeader(bamFile)[[1]][["targets"]]
   which <- GRanges(names(t), IRanges(1, unname(t)))
   which <- which[!grepl("_",as.character(seqnames(which)))]
   which <- which[!as.character(seqnames(which))=='chrM']
   flag <- scanBamFlag(isPaired=TRUE,hasUnmappedMate=FALSE)
+
+## Define function for one chromosome
+
+  procChr <- function(i) {
+    param <- ScanBamParam(flag=flag,what=what, which=which[i], tag='XS')
+    cat("Processing chromosome: ", as.character(seqnames(which[i])), "\n")
+    bam <- scanBam(file=bamFile,param=param)
+    pbam <- procBam(bam=bam, stranded=FALSE, seed=as.integer(seed), verbose=verbose)[[1]]
+    distr <- getDistrs(DB=genomeDB, bam=bam[[1]], verbose=verbose)
+    cat("Removing bam object\n")
+    rm(bam)
+    gc()
+    pc <- pathCounts(reads=pbam, DB=genomeDB, mc.cores=mc.cores, verbose=verbose)
+    if(keep.pbam) {
+      ans <- list(pbam=pbam, distr=distr, pc=pc)
+    } else ans <- list(distr=distr, pc=pc)
+  }
+
+  ## Run for all chromosomes, mclapply or for loop
   if (mc.cores.int>1 ){
     if ('multicore' %in% loadedNamespaces()) {
-
-      ans <- multicore::mclapply(1:length(which), function(i){
-        param <- ScanBamParam(flag=flag,what=what, which=which[i], tag='XS')
-        cat("Processing chromosome: ", as.character(seqnames(which[i])), "\n")
-        bam <- scanBam(file=bamFile,param=param)
-        pbam <- procBam(bam=bam, stranded=FALSE, seed=as.integer(seed), verbose=verbose)[[1]]
-        distr <- getDistrs(DB=genomeDB, bam=bam[[1]], verbose=verbose)
-        rm(bam)
-        gc()
-        pc <- pathCounts(reads=pbam, DB=genomeDB, mc.cores=mc.cores, verbose=verbose)
-        list(pbam=pbam, distr=distr, pc=pc)
-      }, mc.cores=mc.cores.int)
-      
+      ans <- multicore::mclapply(1:length(which), procChr, mc.cores=mc.cores.int, mc.preschedule=FALSE)
     } else stop('multicore library has not been loaded!')
   } else {
-    ans <- lapply(1:length(which), function(i){
-      param <- ScanBamParam(flag=flag,what=what, which=which[i], tag='XS')
-      cat("Processing chromosome: ", as.character(seqnames(which[i])), "\n")
-      bam <- scanBam(file=bamFile,param=param)
-      pbam <- procBam(bam=bam, stranded=FALSE, seed=as.integer(seed), verbose=verbose)[[1]]
-      distr <- getDistrs(DB=genomeDB, bam=bam[[1]], verbose=verbose)
-      rm(bam)
-      gc()
-      pc <- pathCounts(reads=pbam, DB=genomeDB, mc.cores=mc.cores, verbose=verbose)
-      list(pbam=pbam, distr=distr, pc=pc)
-    })
+    ans <- list()
+    for(i in 1:length(which)) ans[[i]] <- procChr(i)
   }
-  allpbam <- lapply(ans, '[[', 'pbam')
+  gc()
+  if(keep.pbam) allpbam <- lapply(ans, "[[", "pbam")
   allpc <- casper:::mergePCWr(ans, genomeDB)
   alldistr <- suppressWarnings(casper:::mergeDisWr(lapply(ans, '[[', 'distr'), lapply(ans, '[[', 'pc')))
   exp <- calcExp(distrs=alldistr, genomeDB=genomeDB, pc=allpc, readLength=readLength, rpkm=rpkm, priorq=priorq, priorqGeneExpr=priorqGeneExpr, citype=citype, niter=niter, burnin=burnin, mc.cores=mc.cores, verbose=verbose)
-  list(pc=allpc, distr=alldistr, exp=exp, pbam=allpbam)
+  if(keep.pbam) {
+    ans <- list(pc=allpc, distr=alldistr, exp=exp, pbam=allpbam)
+  } else ans <- list(pc=allpc, distr=alldistr, exp=exp)
 }
 
